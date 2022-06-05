fd0548ca326cc5da6ec2c673187578a8a74804b1
César Alejandro Garay Bedregal <cgarayb@unsa.edu.pe>
2022-06-05 15:15:54 -0500

    Esta clase nos permitirá crear pilas implementando la interfaz Stack ya creada, los métodos de este fueron agregados pero aún no fueron implementados

---
+++ Pila.java
@@ -0,0 +1,23 @@
+public class Pila<E> implements Stack<E> {
+
+    public boolean empty() {
+        return false;
+    }
+
+    public E peek() {
+        return null;
+    }
+
+    public E pop() {
+        return null;
+    }
+
+    public E push() {
+        return null;
+    }
+
+    public int search() {
+        return 0;
+    }
+
+}
